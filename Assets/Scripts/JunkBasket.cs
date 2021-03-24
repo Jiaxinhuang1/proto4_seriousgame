d209cfdd892c60c15dc7878ac62606fb1b360da8
Jiaxinhuang1 <60531514+Jiaxinhuang1@users.noreply.github.com>
2021-03-24 18:49:16 -0500

    junk collsion

---
+++ Assets/Scripts/JunkBasket.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JunkBasket : MonoBehaviour
+{
+    public UIManager uiManager;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "JunkFood")
+        {
+            uiManager.junkCount += 1;
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text junkText;
+    public Text veggiesText;
+    public Text fruitsText;
+
+    public int junkCount;
+    public int veggiesCount;
+    public int fruitsCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        junkCount = 0;
+        veggiesCount = 0;
+        fruitsCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        junkText.text = "Junk Food: " + junkCount;
+        veggiesText.text = "Veggies: " + veggiesCount;
+        fruitsText.text = "Fruits: " + fruitsCount;
+    }
+}
